3bf607c60254d20c3440311488da9da8b678fd30
Battwo <ANunez2@brooklynsteamcenter.org>
2025-09-11 10:51:13 -0400

    did a lot

---
+++ 01_data_types.py
@@ -0,0 +1,65 @@
+print("Hello")
+
+"""
+TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+Alexannys_float = 12.2
+
+integer= int(Alexannys_float)
+
+print(Alexannys_float)
+print(integer)
+
+
+
+
+"""
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+age = 10
+if age < 0:
+print( negative)
+elif age > 0:
+print(positive)
+else age = 0:
+print(zero)
+
+
+
+
+"""
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+
+
+"""
+TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
+
+"""
+TASK 6:
+
+Create a list of your favorite subjects (as strings). 
+Use the join() function to combine all subjects into a single string separated by commas.
+Then create another version using " - " as the separator.
+Print both the original list and both joined strings.
+"""
