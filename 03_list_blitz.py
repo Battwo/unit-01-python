52e7ab045727bb2fca6a433d0db5b11290ad4f8e
Battwo <ANunez2@brooklynsteamcenter.org>
2025-09-15 10:52:08 -0400

    fire

---
+++ 03_list_blitz.py
@@ -0,0 +1,60 @@
+"""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+["water", "fire", "metel", "air"] # made a list
+
+
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+Elements = ["water", "fire", "metel", "air"] #list of elements
+
+Elements.append ("wood") # added an element
+
+print(Elements) #printed a new element
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+Elements = ["water", "fire", "metel", "air","wood"] #list of elements
+Elements.remove("wood") #removing wood
+print(Elements)
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+Elements = ["water", "fire", "metel", "air","wood"] #list of elements
+Elements[1] = "light" # changing fire to light
+print(Elements)
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+
+
+"""
+Task 7: Slicing lists
+Create a new variable equal to the first 2 items of your list
+Then print out the new variable
+"""
+
+"""
+Task 8: Extend a List
+Extend the list with the elements of another list. Print 
+the updated list.
+"""
+
+names = ["bob", "jake", "emma"]
+names2 = ["kate", "lukcy", "lus"]
