746fccede0901c573988c7d5cd7ae9df0d5284c5
Battwo <ANunez2@brooklynsteamcenter.org>
2025-09-29 10:36:46 -0400

    firelol

--- 07_Quality _inspector.py
+++ 07_Quality _inspector.py
@@ -2,11 +2,11 @@
 
 print()
 print()
-inspector=  input("Enter inspector ID: ")
+inspector=  input("Enter inspector ID: ") # we ask for your id
 
 
-batch_id = input("Enter batch ID: ")
-batch_size = int(input("Enter batch size: "))
+batch_id = input("Enter batch ID: ") # we asked for the id 
+batch_size = int(input("Enter batch size: ")) # the size of the batch 
 
 total_quality_score = 0
 my_dict = { #I made a Dictionary
@@ -18,11 +18,11 @@ my_dict = { #I made a Dictionary
 }
  
 
-for x in range(batch_size):
+for x in range(batch_size): # based on the total of the batch size thw question will repeat as mant times as the batch size is 
      print(x)
 
-     score = float(input("whats the quality score?:"))
+     score = float(input("whats the quality score?:")) # after getting the score of all the items it will add it up and devide it by the batch size 
      total_quality_score += score
 
 
-print(total_quality_score/batch_size)
+print(total_quality_score/batch_size) 
