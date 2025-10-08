514b72cc53258393ab5d1b27601cc498e30b37b1
Battwo <ANunez2@brooklynsteamcenter.org>
2025-10-08 09:24:08 -0400

    fire

--- 08_debugging.py
+++ 08_debugging.py
@@ -19,7 +19,7 @@ print("give me a number") #Determine if numbers 1 to n are even or odd
 n = int(input())#MAYBE HERE TOO 
 
 for num in range(1, n):
-    if num % 2 == 0: #THE ISSUE WAS HERE
+    if num % 2 == 0: #THE ISSUE WAS HERE2
         print(num, "is even.")
     else:
         print(num, "is odd.")
@@ -28,29 +28,34 @@ for num in range(1, n):
 
 
 
+num = int(input("Enter an integer: ")) #I fixed the range to include the number, used commas for printing, and changed the condition to reject all negative numbers. 
+
+if num < 0:
+    print("No negative numbers.")
+else:
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+
+    print("Factorial of", num, "is", result)
+
+
+
 
 attempts = 0
 correct_password = "secret"
+max_attempts = 3
 
 while True:
     password = input("Enter your password: ")
     attempts += 1
 
-    if password == correct_password
-        print("you got the password correct")
-        break
-
-
-    if password == "incorrect_password":
+    if password == correct_password:
         print("Correct password!")
+        break
     else:
         print("Incorrect password")
 
-    if attempts > 3:
+    if attempts >= max_attempts:
         print("Too many attempts")
         break
-
-
-
-
-
